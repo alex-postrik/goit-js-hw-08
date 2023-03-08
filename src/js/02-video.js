9a3adf2d099bc94fe523d73de841d28b7fd57e6d
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-03-08 17:40:23 +0100

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,14 @@
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// const iframeRef = document.querySelector('#vimeo-player');
-// const player = new Player(iframeRef);
-// const KEY_LOCALSTORAGE = 'videoplayer-current-time';
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+const KEY_LOCALSTORAGE = 'videoplayer-current-time';
 
-// function onTimeUpdate(evt) {
-//   localStorage.setItem(KEY_LOCALSTORAGE, evt.seconds);
-// }
+function onTimeUpdate(evt) {
+  localStorage.setItem(KEY_LOCALSTORAGE, evt.seconds);
+}
 
-// player.setCurrentTime(localStorage.getItem(KEY_LOCALSTORAGE) || 0);
+player.setCurrentTime(localStorage.getItem(KEY_LOCALSTORAGE) || 0);
 
-// player.on('timeupdate', throttle(onTimeUpdate, 1000));
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
